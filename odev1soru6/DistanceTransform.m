360e875ce0bf5c21ccbb33d67e4afeb262a161db
elifnurafsar <60623941+elifnurafsar@users.noreply.github.com>
2021-06-09 19:32:12 +0300

    makeup

--- odev1soru6/DistanceTransform.m
+++ odev1soru6/DistanceTransform.m
@@ -1,8 +1,8 @@
 function img = DistanceTransform(I,num)
     [x,y]=size(I);
     distance = I;
-    for r = 1:x-1
-        for c = 1:y-1
+    for r = 1:x
+        for c = 1:y
             x1=r-1;
             if r==1
                 x1=1;
@@ -15,6 +15,12 @@ function img = DistanceTransform(I,num)
             end
             y2=c;
             y3=c+1;
+            if c==y
+                y3=y;
+            end
+            if r==x
+                x3=x;
+            end
             if distance(r,c)==0
                 if I(x1,y1) == num
                     distance(r,c) = num+1;
